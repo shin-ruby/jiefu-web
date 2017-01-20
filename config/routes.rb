475f01e64dae9bc209227fbf0c75c60fea71df6d
shinplus <shin.php@gmail.com>
2017-01-20 15:02:22 +0800

    Add laboratory page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'support' => 'static_pages#support'
   get 'product' => 'static_pages#product'
   get 'product_detail' => 'static_pages#product_detail'
+  get 'laboratory' => 'static_pages#laboratory'
+  get 'support' => 'static_pages#support'
 
   get 'news' => 'static_pages#news'
   get 'news_list' => 'static_pages#news_list'
