b17ae5c4a5bd928e25ae6cb40c250f9a3a877edb
Eric-Guo <guochunzhong@bayekeji.com>
2017-03-22 20:36:30 +0800

    No logic change, just style improvement.

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,33 +1,32 @@
 class Search < ApplicationRecord
-	def products
-		@products ||= find_products
-	end
+  def products
+    @products ||= find_products
+  end
 
-	private
+  private
 
-		def find_products
-			products = Product.order(:name)
-			products = products.where("name like ?", "%#{name}%") if name.present?
-			products = products.where(product_type: product_type) if product_type.present?
-			products = products.where("fo >= ?", min_fo) if min_fo.present?
-			products = products.where("fo <= ?", max_fo) if max_fo.present?
+  def find_products
+    products = Product.order(:name)
+    products = products.where("name like ?", "%#{name}%") if name.present?
+    products = products.where(product_type: product_type) if product_type.present?
+    products = products.where("fo >= ?", min_fo) if min_fo.present?
+    products = products.where("fo <= ?", max_fo) if max_fo.present?
 
-			products = products.where("rated_power >= ?", min_rated_power) if min_rated_power.present?
-			products = products.where("rated_power <= ?", max_rated_power) if max_rated_power.present?
+    products = products.where("rated_power >= ?", min_rated_power) if min_rated_power.present?
+    products = products.where("rated_power <= ?", max_rated_power) if max_rated_power.present?
 
-			products = products.where("sensitivity >= ?", min_sensitivity) if min_sensitivity.present?
-			products = products.where("sensitivity <= ?", max_sensitivity) if max_sensitivity.present?
+    products = products.where("sensitivity >= ?", min_sensitivity) if min_sensitivity.present?
+    products = products.where("sensitivity <= ?", max_sensitivity) if max_sensitivity.present?
 
-			products = products.where("size_length >= ?", min_size_length) if min_size_length.present?
-			products = products.where("size_length <= ?", max_size_length) if max_size_length.present?
+    products = products.where("size_length >= ?", min_size_length) if min_size_length.present?
+    products = products.where("size_length <= ?", max_size_length) if max_size_length.present?
 
-			products = products.where("size_width >= ?", min_size_width) if min_size_width.present?
-			products = products.where("size_width <= ?", max_size_width) if max_size_width.present?
+    products = products.where("size_width >= ?", min_size_width) if min_size_width.present?
+    products = products.where("size_width <= ?", max_size_width) if max_size_width.present?
 
-			products = products.where("size_height >= ?", min_size_height) if min_size_height.present?
-			products = products.where("size_height <= ?", max_size_height) if max_size_height.present?
+    products = products.where("size_height >= ?", min_size_height) if min_size_height.present?
+    products = products.where("size_height <= ?", max_size_height) if max_size_height.present?
 
-
-			products
-		end
+    products
+  end
 end
