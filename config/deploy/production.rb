48ca7af1d024bc8d8278e5d0ce318d1e7bcbe0d5
shinplus <shin.php@gmail.com>
2017-03-24 16:19:43 +0800

    Add capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '120.55.167.141', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
