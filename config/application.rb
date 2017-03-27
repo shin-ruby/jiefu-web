020f2d1732eee0b2ae3e3555c60682ffa593164a
shinplus <shin.php@gmail.com>
2017-03-27 11:22:55 +0800

    Add edit created_at and Fix select category

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,7 @@ module JiefuWeb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.serve_static_assets = true
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
   end
 end
