b7d767ad97af6159bf27746b263a38948946cb6c
shinplus <shin.php@gmail.com>
2017-04-12 11:18:28 +0800

    Add watermark

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,32 @@
 class PhotoUploader < BaseUploader
-	def filename
-    if super.present?
-      "photo/"
-    end
-  end
+ #  def filename
+ #    if super.present?
+ #      "photo/"
+ #    end
+ #  end
+
+  # version :thumb do
+  #   process resize_to_fit: [600, 600]
+  # end
+ 
+  process make_watermark: 'app/assets/images/jiefuailogo.png' 
+
+	def make_watermark(watermark)
+	  manipulate! do |img|
+	    img = img.composite(MiniMagick::Image.open(watermark, 'png')) do |c|
+	      c.gravity 'Center'
+	    end
+	    img = yield(img) if block_given?
+	    img
+	  end
+	end
+
+# first_image = MiniMagick::Image.new("photo.png")
+# second_image = MiniMagick::Image.new("jiefuailogo.png")
+
+# result = first_image.composite(second_image) do |c|
+# 	c.compose "Over"
+# 	c.gravity "Center"
+# end
+
 end
