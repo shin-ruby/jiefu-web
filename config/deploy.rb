7503d160ec420e5ffd7376ca96dc99a2f6d63db5
shinplus <shin.php@gmail.com>
2017-05-10 20:46:58 +0800

    Fix bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/jiefu-web"
 # set :pty, true
 
 set :linked_files, %w{config/database.yml config/secrets.yml db/production.sqlite3}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
