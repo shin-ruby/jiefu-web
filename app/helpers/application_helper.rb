d58b132eb7decb1439cd7e2a4efe46c3be9c6b2f
shinplus <shin.php@gmail.com>
2017-07-04 16:01:04 +0800

    Fix navbar class active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,4 +39,13 @@ module ApplicationHelper
     Product.find_by(name: name)
   end
 
+  def is_active_controller(controller_name)
+    params[:controller] == controller_name ? "active" : nil
+  end
+
+  def is_active_action(action_name)
+    params[:action] == action_name ? "active" : nil
+  end
+
+
 end
