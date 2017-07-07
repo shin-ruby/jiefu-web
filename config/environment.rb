d2e6ff835644af490630c814b9279ccd734f10c8
shinplus <shin.php@gmail.com>
2017-07-07 17:24:44 +0800

    Fix will pageinate i18n

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,5 @@ Rails.application.initialize!
 
 require 'will_paginate'
 # will_paginate custom label
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = '« 上一页'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = '下一页 »'
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '<<'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '>>'
